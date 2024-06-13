d7f7a6c27baa1940667535d63abf1c8494049c0e
Federico Herrera Modernizacion <fedeeherrera2001@gmail.com>
2024-06-13 10:52:23 -0300

    Validaciones Formulario, creacion de estados y mas

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,34 @@
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { Activity } from '../types'
 import { categories } from '../data/categories'
 
 export default function Form() {
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: '',
+    calories: 0,
+  })
+
+  const handleChange = (e : ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
+    const isNumberField = ['category', 'calories'].includes(e.target.id)
+    setActivity({
+      ...activity,
+      [e.target.id] : isNumberField ? +e.target.value : e.target.value
+    })
+  }
+
+  const isValidActivity = () => {
+    const {name, calories} = activity
+    return name.trim() !== '' && calories > 0
+  }
+
+  const handleSubmit = (e : FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
+
   return (
     <>
-      <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+      <form className="space-y-5 bg-white shadow p-10 rounded-lg" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-3">
           <label htmlFor="category" className="font-bold">
             {' '}
@@ -11,8 +36,9 @@ export default function Form() {
           </label>
           <select
             className="border border-slate-300 rounded-lg bg-white p-2 w-full"
-            name="category"
             id="category"
+            value={activity.category}
+            onChange={handleChange}
           >
             {categories.map((category) => (
               <option key={category.id} value={category.id}>
@@ -21,20 +47,42 @@ export default function Form() {
             ))}
           </select>
         </div>
+
         <div className="grid grid-cols-1 gap-3">
-          <label htmlFor="activity" className="font-bold">
+          <label htmlFor="name" className="font-bold">
             Actividad:
           </label>
-          <input className='border border-slate-300 p-2 rounded-lg' type="text" name="activity" id="activity" placeholder='Ej. Comida, Jugo de Naranja, Ejercicio, Ensalada, Pesas, Etc'/>
+          <input
+            value={activity.name}
+            className="border border-slate-300 p-2 rounded-lg"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Ej. Comida, Jugo de Naranja, Ejercicio, Ensalada, Pesas, Etc"
+            onChange={handleChange}
+          />
         </div>
 
         <div className="grid grid-cols-1 gap-3">
           <label htmlFor="calories" className="font-bold">
             Calorias:
           </label>
-          <input className='border border-slate-300 p-2 rounded-lg' type="number" name="calories" id="calories" placeholder='Calorias Ej. 300 O 500'/>
+          <input
+            value={activity.calories}
+            className="border border-slate-300 p-2 rounded-lg"
+            type="number"
+            name="calories"
+            id="calories"
+            placeholder="Calorias Ej. 300 O 500"
+            onChange={handleChange}
+          />
         </div>
-        <input type="submit" className='bg-gray-800 hover:bg-gay-900 w-full p-2 font-bold uppercase text-white cursor-pointer'value="Guardar Comida o Guardar Ejercicio" />
+        <input
+          disabled={!isValidActivity()}
+          type="submit"
+          className="bg-gray-800 hover:bg-gay-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+          value={activity.category === 1 ? 'Guardar Comida' : 'Guardar Ejercicio'}
+        />
       </form>
     </>
   )
