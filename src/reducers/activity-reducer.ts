c61e743accf8d595467bfee771c885431bef787b
Fedeeherrera <fedeeherrera2001@gmail.com>
2024-06-29 17:03:12 -0300

    Creacion de Reiniciar App y button

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -4,14 +4,20 @@ export type ActivityActions =
   | { type: 'save-activity'; payload: { newActivity: Activity } }
   | { type: 'set-activeId'; payload: { id: Activity['id'] } }
   | { type: 'delete-activity'; payload: { id: Activity['id'] } }
+  | { type: 'restart-app' }
 
 export type ActivityState = {
   activities: Activity[]
   activeId: Activity['id']
 }
 
+const localStorageActivities = (): Activity[] => {
+  const activities = localStorage.getItem('activities')
+  return activities ? JSON.parse(activities) : []
+}
+
 export const initialState: ActivityState = {
-  activities: [],
+  activities: localStorageActivities(),
   activeId: '',
 }
 
@@ -47,7 +53,16 @@ export const activityRecucer = (
   if (action.type === 'delete-activity') {
     return {
       ...state,
-      activities: state.activities.filter(activity => activity.id !== action.payload.id)
+      activities: state.activities.filter(
+        (activity) => activity.id !== action.payload.id
+      ),
+    }
+  }
+
+  if (action.type === 'restart-app') {
+    return {
+      activities: [],
+      activeId: '',
     }
   }
 
